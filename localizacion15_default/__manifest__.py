9535652639265d9679150a4fa9427202ccf26040
filoquin <filoquin@gmail.com>
2022-06-21 19:40:22 -0300

    [FIX] Corregir nombre del repo

--- localizacion15_default/__manifest__.py
+++ localizacion15_default/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 
 {
-    'name': 'Localizacion',
+    'name': 'Localizacion15',
     'version': '15.0.1.0.0',
     'category': 'Tools',
     'summary': "Test for v15 CE",
